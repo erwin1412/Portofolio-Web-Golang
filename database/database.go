021189c0fdf467a6735c76fab0f9900912610e41
suryaa6666 <suryaelidanto@gmail.com>
2023-08-23 09:32:57 +0700

    fix : database name

--- database/database.go
+++ database/database.go
@@ -12,7 +12,7 @@ var Conn *pgx.Conn
 
 func ConectDatabase() {
 	var err error
-	dbUrl := "postgres://postgres:root@localhost:5432/myapp"
+	dbUrl := "postgres://postgres:root@localhost:5432/tugas16"
 	Conn, err = pgx.Connect(context.Background(), dbUrl)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
